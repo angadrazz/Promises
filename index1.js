bca6171a657010e19896ffa59dd45c6e89ee8723
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2023-01-02 17:20:38 +0530

    abc

--- index1.js
+++ index1.js
@@ -43,4 +43,14 @@ function doATask(callback) {
 const promise = new Promise((resolve,reject) => {
                 resolve(1)
                 })
+
+
+                const promise1 = new Promise((resolve,reject) => { setTimeout(() => { resolve('a') }) })
+
+                const promise2 = new Promise((resolve,reject) => { resolve('b') })
+                
+                const promise3 = new Promise((resolve,reject) => { setTimeout(() => { resolve('c') },10) })
+                
+                Promise.all([promise1,promise2,promise3]) .then((msg) => { console.log(msg) })
+                
                 
