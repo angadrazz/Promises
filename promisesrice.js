467b8f843a1336d57f39b4ece2381ac6083d6bbe
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2023-01-02 18:46:43 +0530

    function randomAsyncFuntion() {         const time = Math.floor(Math.random * 100)         const x = setTimeout(() => {                 console.log('finished!!')         }, time) }
    async function asyncWithCallback(callback) {
            randomAsyncFunction()
            setTimeout(() => {
                    callback()
            }, 5000)
    }
    
    asyncWithCallback(() => {
            console.log("angadwithasyncfunction")
    })

--- promisesrice.js
+++ promisesrice.js
@@ -39,3 +39,54 @@ const promise1 = new Promise((resolve,reject) => {
       .then((msg) => {
         console.log(msg)
       })
+
+
+const delay = (time) => {
+        return new Promise((resolve, reject) => {
+                setTimeout(resolve, time)
+        })
+}
+
+const counter = () => {
+        let time = 0
+        let pause = false
+        return {
+                start: async function start() {
+                        for(let i = time;;i++){
+                                time++;
+                                if(pause) break
+                                console.log(i)
+                                await delay(1000)
+                        }
+                },
+                stop: function stop() {
+                        pause = true
+                }
+        }
+}
+
+let count = counter()
+
+count.start()
+setTimeout(() => {
+        count.stop()
+}, 6000)
+
+
+function randomAsyncFuntion() {
+        const time = Math.floor(Math.random * 100)
+        const x = setTimeout(() => {
+                console.log('finished!!')
+        }, time)
+}
+
+async function asyncWithCallback(callback) {
+        randomAsyncFunction()
+        setTimeout(() => {
+                callback()
+        }, 5000)
+}
+
+asyncWithCallback(() => {
+        console.log("angadwithasyncfunction")
+})
