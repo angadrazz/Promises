db34066bd5154937f78efde7434883e577a6c326
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2023-01-02 17:48:37 +0530

    js

--- index1.js
+++ index1.js
@@ -92,3 +92,15 @@ const promise = new Promise((resolve,reject) => {
                       }
                       
                       log()
+
+                      const promise = new Promise((resolve,reject) => {
+                        resolve('a')
+                      })
+                      
+                      setTimeout(() => {
+                        console.log('b')
+                      })
+                      
+                      promise.then((msg) => {
+                        console.log(msg)
+                      })
